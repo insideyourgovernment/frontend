1f934b5366935ee675178594ac48336aea09cf90
Tim Clemans <tim@insideyourgovernment.com>
2015-12-22 21:17:39 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -244,7 +244,7 @@ function addData(table, element, data, page) {
 }
 function addUrl(element, url) {
     console.log('url', url);
-  window.location.hash = '!/information/'+url.substring(52);
+  window.location.hash = '!/information/'+url.substring(45);
   //console.log('add url '+url);
   setTimeout(function() {element.find('.url').html('<a href="'+url+'">'+url+'</a>');}, 500);
 }
