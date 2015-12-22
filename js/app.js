6bc2efd189f943ca7d1fe93814eb3dae2ca9df63
Tim Clemans <tim@insideyourgovernment.com>
2015-12-22 20:14:43 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -385,52 +385,30 @@ function _init() {
    * @type Function
    * @Usage: $.AdminLTE.tree('.sidebar')
    */
-  $.AdminLTE.tree = function (menu) {
-    var _this = this;
-    var animationSpeed = $.AdminLTE.options.animationSpeed;
-    $(document).on('click', menu + ' li a', function (e) {
-      //Get the clicked link and the next element
-      var $this = $(this);
-      var checkElement = $this.next();
-
-      //Check if the next element is a menu and is visible
-      if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible'))) {
-        //Close the menu
-        checkElement.slideUp(animationSpeed, function () {
-          checkElement.removeClass('menu-open');
-          //Fix the layout in case the sidebar stretches over the height of the window
-          //_this.layout.fix();
-        });
-        checkElement.parent("li").removeClass("active");
+  $.AdminLTE.tree = function(a) {
+    var b = this,
+  c = $.AdminLTE.options.animationSpeed;
+    $(document).on("click", a + " li a", function(a) {
+    var d = $(this);
+    var e;
+    if (d.attr('href') == '#') {
+      e = d.next();
+      if (e.is(".treeview-menu") && e.is(":visible")) e.slideUp(c, function() {
+        e.removeClass("menu-open")
+      }), e.parent("li").removeClass("active");
+      else if (e.is(".treeview-menu") && !e.is(":visible")) {
+            var f = d.parents("ul").first(),
+        g = f.find("ul:visible").slideUp(c);
+        g.removeClass("menu-open");
+        var h = d.parent("li");
+        e.slideDown(c, function() {
+          e.addClass("menu-open"), f.find("li.active").removeClass("active"), h.addClass("active"), b.layout.fix()
+        })
       }
-      //If the menu is not visible
-      else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
-        //Get the parent menu
-        var parent = $this.parents('ul').first();
-        //Close all open menus within the parent
-        var ul = parent.find('ul:visible').slideUp(animationSpeed);
-        //Remove the menu-open class from the parent
-        ul.removeClass('menu-open');
-        //Get the parent li
-        var parent_li = $this.parent("li");
-
-        //Open the target menu and add the menu-open class
-        checkElement.slideDown(animationSpeed, function () {
-          //Add the class active to the parent li
-          checkElement.addClass('menu-open');
-          parent.find('li.active').removeClass('active');
-          parent_li.addClass('active');
-          //Fix the layout in case the sidebar stretches over the height of the window
-          _this.layout.fix();
-        });
-      }
-      //if this isn't a link, prevent the page from being redirected
-      //if (checkElement.is('.treeview-menu')) {
-      //  e.preventDefault();
-      //}
-    });
-  };
-
+      e.is(".treeview-menu") && a.preventDefault()
+    }
+    })
+}
   /* ControlSidebar
    * ==============
    * Adds functionality to the right sidebar
