cbb3be3d7e1946babc974c3bcf32c6390032a589
Tim Clemans <tim@insideyourgovernment.com>
2015-12-26 19:52:29 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -476,6 +476,7 @@ $(function() {
         window.ws.onopen = function() { 
             processHash();
             console.log("Connection is opened");
+           window.ws.send(JSON.stringify({'ws_for': 'count', 'table': 'police_response_events'}));
            window.ws.send(JSON.stringify({'ws_for': 'change_data_for_id', 'table': 'site_content', 'get': 'site_name'}));
             window.ws.send(JSON.stringify({'ws_for': 'change_data_for_id', 'table': 'site_content', 'get': 'site_acronym'}));
             window.ws.send(JSON.stringify({'ws_for': 'change_data_for_id', 'table': 'site_content', 'get': 'site_tag_line'}));
